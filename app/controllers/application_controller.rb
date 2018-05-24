ddb2f7db04cfcbcb3cc5ed242f657200c1ff7f77
pacafs <paca.fs@gmail.com>
2018-05-24 19:36:17 +0100

    added render nothing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,13 @@ class ApplicationController < ActionController::Base
   private
 
   def check_user_ip
-    return head :ok, content_type: "text/html" unless check_ip === true
+    if check_ip
+        return head :ok, content_type: "text/html"
+    end
   end
 
   def check_ip
-    if cookies['AllowUserIP']
-        puts cookies['AllowUserIP']
-        return cookies['AllowUserIP']
-    else
+    unless cookies['AllowUserIP']
         europe_blacklist = ['AT','BE','BG','HR','CY','CZ','DK','EE',
                             'FI','FR','DE','GR','HU','IE','IT','LV',
                             'LT','LU','MT','NL','PL','PT','RO','SK',
@@ -26,10 +25,10 @@ class ApplicationController < ActionController::Base
         response = HTTParty.get('http://pro.ip-api.com/json/'+request.remote_ip+'?key=1freiZwhIniYh5w')
         ip = europe_blacklist.any? { |e| e === response['countryCode'] }
         ip ? cookies['AllowUserIP'] = false : cookies['AllowUserIP'] = true
-        puts cookies['AllowUserIP']
-        return cookies['AllowUserIP']
     end
-
+    return cookies['AllowUserIP']
   end
+
+
 end
 
