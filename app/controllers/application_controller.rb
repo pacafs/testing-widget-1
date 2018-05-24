0c414b0b76f6ee8b3c77805f7b72f1f9affffff8
pacafs <paca.fs@gmail.com>
2018-05-24 19:19:15 +0100

    added render nothing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,20 +10,24 @@ class ApplicationController < ActionController::Base
   end
 
   def check_ip
-    return cookies['AllowUserIP'] unless !cookies['AllowUserIP']
-    europe_blacklist = ['AT','BE','BG','HR','CY','CZ','DK','EE',
-                        'FI','FR','DE','GR','HU','IE','IT','LV',
-                        'LT','LU','MT','NL','PL','PT','RO','SK',
-                        'SI','ES','SE','GB','GF','GP','MQ','ME',
-                        'YT','RE','MF','GI','AX','PM','GL','BL',
-                        'SX','AW','CW','WF','PF','NC','TF','AI',
-                        'BM','IO','VG','KY','FK','MS','PN','SH',
-                        'GS','TC','AD','LI','MC','SM','VA','JE',
-                        'GG','GI']
-    response = HTTParty.get('http://pro.ip-api.com/json/'+request.remote_ip+'?key=1freiZwhIniYh5w')
-    ip = europe_blacklist.any? { |e| e === response['countryCode'] }
-    ip ? cookies['AllowUserIP'] = false : cookies['AllowUserIP'] = true
-    return cookies['AllowUserIP']
+    if cookies['AllowUserIP']
+        puts cookies['AllowUserIP']
+        return cookies['AllowUserIP']
+    else
+        europe_blacklist = ['AT','BE','BG','HR','CY','CZ','DK','EE',
+                            'FI','FR','DE','GR','HU','IE','IT','LV',
+                            'LT','LU','MT','NL','PL','PT','RO','SK',
+                            'SI','ES','SE','GB','GF','GP','MQ','ME',
+                            'YT','RE','MF','GI','AX','PM','GL','BL',
+                            'SX','AW','CW','WF','PF','NC','TF','AI',
+                            'BM','IO','VG','KY','FK','MS','PN','SH',
+                            'GS','TC','AD','LI','MC','SM','VA','JE',
+                            'GG','GI']
+        response = HTTParty.get('http://pro.ip-api.com/json/'+request.remote_ip+'?key=1freiZwhIniYh5w')
+        ip = europe_blacklist.any? { |e| e === response['countryCode'] }
+        ip ? cookies['AllowUserIP'] = false : cookies['AllowUserIP'] = true
+        return cookies['AllowUserIP']
+    end
 
   end
 end
